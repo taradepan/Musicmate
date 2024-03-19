fad564cb3b0ad02f64066f260918c34c130ad4d6
taradepan <taradepan>
2024-03-19 23:02:28 +0530

    Add .env and venv to .gitignore, update requirements.txt, and add code to spoti.ipynb

---
+++ chat.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+def generate_response(prompt):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="gemma-7b-it",
+    )
+
+    return chat_completion.choices[0].message.content
+
+print(generate_response("Hello"))
